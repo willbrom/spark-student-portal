29cd501ded687f8603e91b130dfbdddc2af2340a
willbrom <alisajid552@yahoo.com>
2019-02-27 16:32:12 +0300

    add sign-in

--- src/main/java/com/willbrom/spark/example/Main.java
+++ src/main/java/com/willbrom/spark/example/Main.java
@@ -3,12 +3,23 @@ package com.willbrom.spark.example;
 import spark.ModelAndView;
 import spark.template.handlebars.HandlebarsTemplateEngine;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import static spark.Spark.*;
 
 public class Main {
+
     public static void main(String[] args) {
         get("/hello/:name", (request, response) -> request.params("name"));
-
         get("/", (request, response) -> new ModelAndView(null, "index.hbs"), new HandlebarsTemplateEngine());
+
+        post("/sign-in", ((request, response) -> {
+            Map<String, String> model = new HashMap<>();
+            model.put("username", request.queryParams("username"));
+
+            return new ModelAndView(model, "sign-in.hbs");
+
+        }), new HandlebarsTemplateEngine());
     }
 }
